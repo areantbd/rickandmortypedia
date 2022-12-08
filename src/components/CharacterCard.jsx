8e7fec59a2a09c2c873741c40b99f09667ba1fe2
Iván <ivantbd87@gmail.com>
2022-12-08 12:14:01 +0100

    feat: pagination, characters cards and background

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CharacterCard({name, status, species, gender, image, type}) {
   return (
     <div className='container'>
-        <h2>{name}</h2>
+        {/* <h2>{name}</h2>
         <div className='d-flex flex-row gap-4'>
           <img src={`${image}`} alt={name}/>
           <div>
@@ -13,7 +13,21 @@ function CharacterCard({name, status, species, gender, image, type}) {
             <h3>Type: {type}</h3>
           </div>
         </div>
-        <hr/>
+        <hr/> */}
+
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-6">
+              <img src={`${image}`} className="img-fluid rounded-start" alt={name} />
+            </div>
+            <div className="col-6 my-auto">
+              <div className="card-body">
+                <h2 className="card-title mb-5">{name}</h2>
+                <h3 className="card-text">Specie: {species}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
--- src/screens/CharactersScreen.jsx
+++ src/screens/CharactersScreen.jsx
@@ -4,21 +4,49 @@ import CharacterCard from '../components/CharacterCard';
 
 function CharactersScreen() {
     const [chars, setChars] = useState(null)
+    const [actualPage, setPage] = useState(1)
+    
 
     useEffect(() => {
-        getCharacters()
+        getCharacters({
+          page: actualPage,
+          status: ""
+        })
             .then((data) => setChars(data))
             .catch((error) => console.error(error));
-    }, [])
-    console.log(chars)
+    }, [actualPage])
+
+    if (actualPage < 1) {
+      setPage(1)
+    }
+    
+    console.log(actualPage)
+    function handleUp() {      
+      setPage(actualPage + 1)
+    }
+    function handleDown() {      
+      setPage(actualPage - 1)
+    }
   return (
-    <div>
-    <h1>Rick & Morty Characters</h1>
-    <h3>Total characters: {chars?.data?.info?.count}</h3>
-    <h3 className='mb-5'>Total pages: {chars?.data?.info?.pages}</h3>
+    <div className='container pad-bot'>
+    <h1 className='text-center my-5 text-light'>Rick & Morty Characters</h1>
+    <div className='d-flex justify-content-between border px-3 py-2 text-light'>
+      <h3>Total characters: {chars?.data?.info?.count}</h3>
+      <h3>Total pages: {chars?.data?.info?.pages}</h3>
+    </div>
+    <div className='d-flex justify-content-center gap-5 my-5'>
+      {actualPage > 1 && <button onClick={handleDown} className="btn btn-sm btn-secondary">Page down</button>}
+      <h3 className='text-light'>{actualPage}</h3>
+      <button onClick={handleUp} className="btn btn-sm btn-secondary">Page up</button>
+    </div>
     {chars?.data?.results?.map((char) => (
       <CharacterCard {...char} key={char.id}/>
     ))}
+    <div className='d-flex justify-content-center gap-5 my-5'>
+      {actualPage > 1 && <button onClick={handleDown} className="btn btn-sm btn-secondary">Page down</button>}
+      <h3 className='text-light'>{actualPage}</h3>
+      <button onClick={handleUp} className="btn btn-sm btn-secondary">Page up</button>
+    </div>
     
     </div>
   )
