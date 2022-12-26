54cb134a6dd94135634e43e4bfebbddcbbbb89c4
Iván <ivantbd87@gmail.com>
2022-12-26 15:28:44 +0100

    minor

--- src/components/characters-card/CharacterCard.jsx
+++ src/components/characters-card/CharacterCard.jsx
@@ -2,42 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function CharacterCard({name, status, species, gender, image, type, id}) {
-  return (
-    <div className='container col-12 col-md-5'>
-        {/* <h2>{name}</h2>
-        <div className='d-flex flex-row gap-4'>
-          <img src={`${image}`} alt={name}/>
-          <div>
-            <h3>Status: {status}</h3>
-            <h3>Specie: {species}</h3>
-            <h3>Gender: {gender}</h3>
-            <h3>Type: {type}</h3>
-          </div>
-        </div>
-        <hr/> */}
 
+  if (species === "Mythological Creature") {
+    species = "mythologic"
+  }
+
+  return (
+    <div className='container col-12 col-md-6'>
         <Link to={`/character/${id}`} className=" text-decoration-none text-dark">
-        <div className="card mb-3 border-0 chars-card-body ">
-              {gender === "Male" && status === "Alive" &&  species !== "Animal" && <div className='status'>👨</div>}
-              {gender === "Female" && status === "Alive" &&  species !== "Animal" && <div className='status'>👩</div>}
-              {status === "Dead" && <div className='status'>💀</div>}
-              {gender === "Male" && status === "unknown" &&  species !== "Animal" && <div className='status'>🤷‍♂️</div>}
-              {gender === "Female" && status === "unknown" &&  species !== "Animal" && <div className='status'>🤷‍♀️</div>}
-              {gender === "unknown" &&  status !== "Dead" && <div className='status'>👽</div>}
-              {gender === "Genderless" &&  status !== "Dead" && <div className='status'>👽</div>}
-			        {species === "Animal" && status !== "Dead" && <div className='status'>🐶</div>}
-          <div className="row g-0">
-            <div className="col-6">
-              <img src={`${image}`} className="img-fluid rounded-start" alt={name} />
-            </div>
-            <div className="col-6 my-auto">
-              <div className="card-body ms-2 ">
-                <h5 className="card-title mb-3"><b>{name}</b></h5>
-                <h6 className="card-text">{species}</h6>
+          <div className="card mb-3 border-0 chars-card-body ">
+            <div className="row g-0">
+              <div className="col-6">
+                <img src={`${image}`} className="img-fluid rounded-start" alt={name} />
+              </div>
+              <div className="col-6 my-auto">
+                {status === "Alive" && <div className='status  '><b> {species.toLowerCase( )} - {status.toLowerCase()}</b>🟢</div>}
+                {status === "Dead" && <div className='status'><b>{species.toLowerCase( )} - {status.toLowerCase()}</b>🔴</div>}
+                {status === "unknown" && <div className='status '><b> {species.toLowerCase( )} - {status.toLowerCase()}</b>⚫</div>}
+                <div className="card-body ms-2 ">
+                  <h5 className="card-title mb-3"><b>{name}</b></h5>
+                  <h6 className="card-text">{gender}</h6>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </Link>
     </div>
   )
